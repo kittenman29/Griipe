982b43ccfdb441c00de087100e2dfbeb6c8d8ece
Will Schulz <will.schulz29@gmail.com>
2019-05-29 22:37:07 -0400

    almost done with heroku

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,35 @@
 
-require('dotenv').config()
+// require('dotenv').config()
+
+const localPgConnection = {
+  host: 'localhost',
+  database: 'griipe'
+}
+
+const prodDBConnection = process.env.DATABASE_URL || localPgConnection
 
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './data/griipe.db3' }, // change this if you want a different name for the data
-    useNullAsDefault: true, // used to avoid warning on console
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'dbmigrations',
+    connection: {
+      filename: './data/griipe.db3',
     },
-    seeds: { directory: './data/seeds' },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      }
-    }
-  },
-
-  production : {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations',
     },
-    
     seeds: {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: prodDBConnection, // an object or a string
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
 };
